50b0c0a18b4be8e93c62ca504b5e915a06f2fab6
CarlosDZ <carlos.dz16180@gmail.com>
2026-02-02 19:06:07 +0100

    FIX: Mailer works and doesnt go to spam

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,6 +2,6 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 const pkg = JSON.parse(readFileSync(join(process.cwd(), 'package.json'), 'utf8'));
 export const AppConfig = {
-    baseUrl: pkg.appConfig.baseUrl as string,
-    emailDomain: pkg.appConfig.emailFrom as string
+    baseUrl: pkg.AppConfig.baseUrl as string,
+    emailDomain: pkg.AppConfig.emailDomain as string
 };
