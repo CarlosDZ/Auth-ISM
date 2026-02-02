1c97337c2fcfa8c0fbce4a3616001934d36496a5
CarlosDZ <carlos.dz16180@gmail.com>
2026-02-02 14:21:11 +0100

    FIX: Consistency fix on module imports

--- src/auth/email-verification.module.ts
+++ src/auth/email-verification.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { EmailVerificationController } from './email-verification.controller';
 import { EmailVerificationService } from './email-verification.service';
+import { TenantsModule } from 'src/tenants/tenants.module';
 
 @Module({
+    imports: [TenantsModule],
     controllers: [EmailVerificationController],
     providers: [EmailVerificationService, PrismaService],
     exports: [EmailVerificationService]
