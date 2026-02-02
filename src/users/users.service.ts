8f70886ebbf36d4c17498f459ac3ef34e89b4e44
CarlosDZ <carlos.dz16180@gmail.com>
2026-02-02 05:16:16 +0100

    SERVICE: User register on tenant

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,17 @@
-import { Injectable } from "@nestjs/common";
-import { PasswordHasher } from "./security/password-hasher.service";
-import { Prisma } from "@prisma/client";
-import { CreateFirstTenantAdminDto } from "src/tenants/dto/create-first-tenant-admin.dto";
-import { User } from "@prisma/client";
+import { Injectable } from '@nestjs/common';
+import { PasswordHasher } from './security/password-hasher.service';
+import { Prisma } from '@prisma/client';
+import { CreateFirstTenantAdminDto } from 'src/tenants/dto/create-first-tenant-admin.dto';
+import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly passwordHasher: PasswordHasher) {}
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly passwordHasher: PasswordHasher
+    ) {}
 
     async createTenantAdmin(
         tx: Prisma.TransactionClient,
@@ -21,6 +26,19 @@ export class UsersService {
                 password: hashed,
                 name: admin.name,
                 tenantId
+            }
+        });
+    }
+
+    async registerTenantUser(tenantId: string, dto: CreateUserDto) {
+        const hashed = await this.passwordHasher.hash(dto.password);
+
+        return await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                name: dto.name,
+                password: hashed,
+                tenantId: tenantId
             }
         });
     }
