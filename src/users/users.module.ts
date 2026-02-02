d6aa068e8a20c9bb5edc0ab3c6420836796fad21
CarlosDZ <carlos.dz16180@gmail.com>
2026-02-02 13:43:35 +0100

    FIX: Minor fix on Users Module Providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SecurityModule } from './security/security.module';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
     imports: [SecurityModule],
-    providers: [UsersService],
+    providers: [UsersService, PrismaService],
     exports: [UsersService]
 })
 export class UsersModule {}
