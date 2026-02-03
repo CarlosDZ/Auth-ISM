43a4b7d87981914bd10259749d47cd2d566868ae
CarlosDZ <carlos.dz16180@gmail.com>
2026-02-03 03:42:55 +0100

    FIX: Minor fix

--- src/auth/guards/guards.module.ts
+++ src/auth/guards/guards.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from 'src/users/users.module';
 import { RolesModule } from 'src/roles/roles.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UtilsModule } from 'src/utils/utils.module';
